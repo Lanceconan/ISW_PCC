40321de5e7d4844e7618e4a72ecd5f0bfc8ca2a9
Daniel Gutierrez <conan_022@hotmail.com>
2014-06-20 20:28:41 -0400

    Cambios para Login
    Se cambiaron algunas caracteristicas del Index

--- app/views/test/index.blade.php
+++ app/views/test/index.blade.php
@@ -11,13 +11,37 @@
 <body>
 <div class="principal">
 <h1>Planificaci&oacuten semestral para asignaturas de la UTEM</h1>
+ 
+<div id="Datos">
+<form method="post" action="index.php">
+        <table >
+            <br>
+            <tr> 
+                    <td><label>Usuario:</label></td>
+                    <td><input type="text" name="usuario" required></input></td>
+            </tr> 
+            <tr> 
+                    <td><label>Contraseña:</label></td>
+                    <td><input type="text" name="rut" required></input></td>
+                    <td></td>
+            <tr> 
+                    <td><input type="submit" value ="Ingresar" required></input></td>
+            </tr> 
+            </tr>        
+        </table>
+</form>
+</div>
+
 <div id="foot">
-	<p>UNIVERSIDAD TECNOLOGICA METROPOLITANA</p>
+	<p>UNIVERSIDAD TECNOL&OacuteGICA METROPOLITANA</p>
 	<p>Dieciocho 161, Santiago-Chile</p>
 	<div id="foot2">Visite www.&#32.&#32.&#32.cl
 	<div id="logo"></div></div>
+	
 </div>
 </div>
 </body>
+
+
 </html>
 @stop
