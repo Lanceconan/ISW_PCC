d8957040e3a83d933aeb61536bbe025c7cf1be23
Ricardo López Núñez <rlopezn@outlook.cl>
2014-06-27 14:43:34 -0400

    cambio css y nombre

--- app/views/configurar/indexAdmin.blade.php
+++ app/views/configurar/indexAdmin.blade.php
@@ -5,7 +5,7 @@
 @section('javascript')
 <?php echo HTML::script('Js/funciones.js');?>
 @section('css')
-<?php echo HTML::style('Css/styles4.css');?>
+<?php echo HTML::style('Css/stylesAdmin.css');?>
 <html>
 
 <body>
