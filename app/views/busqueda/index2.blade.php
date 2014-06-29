85c1722aec547eca3845e534c6aac9b43817ecc4
Famfrit <cguerrero@icci.cl>
2014-06-28 20:35:15 -0400

    Nuevas Vistas

---
+++ app/views/busqueda/index2.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.master')
+@section('titulo')
+	Portal de Planificacion de Asignaturas
+@stop
+@section('javascript')
+<?php echo HTML::script('Js/funciones.js');?>
+@section('css')
+<?php echo HTML::style('Css/styles2.css');?>
+<html>
+
+<body>
+<div class="principal">
+<h1>B&uacutesqueda</h1>
+	<div class="menu">
+	<ul>
+		<li>{{ HTML::link('crear', 'Crear Plan') }}</li>
+		<li>{{ HTML::link('revisar', 'Revisar Plan')}}</li>
+		<li>{{ HTML::link('reporte', 'Generar PDF')}}</li>
+		<li>{{ HTML::link('buscar', 'Buscar...')}}</li>
+		<li>{{ HTML::link('opciones', 'Config. Cuenta')}}</li>
+		<li>{{ HTML::link('logout', 'Salir')}}</li>
+	</ul>
+	</div>
+	<div class="busqueda">
+		<p>Buscar por:</p>
+		<p>{{Form::open(array('url' => 'buscar'))}}</p>
+		<p>{{Form::label('apellido','Apellido Profesor')}}</p>
+		<p>{{Form::text('apellido')}}</p>
+		<p>{{Form::label('nombre','Nombre Profesor')}}</p>
+		<p>{{Form::text('nombre')}}</p>
+		<p>{{Form::label('asignatura','Asignatura')}}</p>
+		<p>{{Form::text('ramo')}}</p>
+		<p>{{Form::label('departamento','Departamento')}}</p>
+		<p>{{Form::text('depto')}}</p>
+		<p>{{Form::submit('Buscar')}}</p>
+		{{Form::close()}}
+	</div>
+	
+	<div id="foot">
+		<p>UNIVERSIDAD TECNOLOGICA METROPOLITANA</p>
+		<p>Dieciocho 161, Santiago-Chile</p>
+		<div id="link-logo">
+			<a href=http://www.utem.cl><img src ="Images/logo.png"></a>
+		</div>
+	</div>
+	<div id="linea">
+	</div>
+</div>
+</body>
+</html>
+@stop
---
+++ app/views/busqueda/index4.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.master')
+@section('titulo')
+	Portal de Planificacion de Asignaturas
+@stop
+@section('javascript')
+<?php echo HTML::script('Js/funciones.js');?>
+@section('css')
+<?php echo HTML::style('Css/styles4.css');?>
+<html>
+
+<body>
+<div class="principal">
+	<h1>B&uacutesqueda</h1>
+	<div class="menu">
+	<ul>
+		<li><a href="#">Manejo de cuentas</a>
+		<ul>
+			<li><a href="#">Agregar</a></li>
+			<li><a href="#">Reasignar</a></li>
+			<li><a href="#">Borrar</a></li>
+		</ul>
+		</li>
+		<li><a href="#">Buscar...</a></li>
+		<li><a href="#">Config. cuenta</a></li>
+		<li><a href="#">Salir</a></li>
+	</ul>
+	</div>
+
+	<div class="busqueda">
+		<p>Buscar por:</p>
+		<p>{{Form::open(array('url' => 'buscar'))}}</p>
+		<p>{{Form::label('apellido','Apellido Profesor')}}</p>
+		<p>{{Form::text('apellido')}}</p>
+		<p>{{Form::label('nombre','Nombre Profesor')}}</p>
+		<p>{{Form::text('nombre')}}</p>
+		<p>{{Form::label('asignatura','Asignatura')}}</p>
+		<p>{{Form::text('ramo')}}</p>
+		<p>{{Form::label('departamento','Departamento')}}</p>
+		<p>{{Form::text('depto')}}</p>
+		<p>{{Form::submit('Buscar')}}</p>
+		{{Form::close()}}
+	</div>
+	
+	<div id="foot">
+		<p>UNIVERSIDAD TECNOLOGICA METROPOLITANA</p>
+		<p>Dieciocho 161, Santiago-Chile</p>
+		<div id="link-logo">
+			<a href=http://www.utem.cl><img src ="Images/logo.png"></a>
+		</div>
+	</div>
+	<div id="linea">
+	</div>
+</div>
+</body>
+</html>
+@stop
