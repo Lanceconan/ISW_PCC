98b5099e29ce9898daa28b482f236432b12a872f
rlopezn <rlopez@icci.cl>
2014-06-29 22:16:17 -0400

    cambio

--- app/views/login/index.blade.php
+++ app/views/login/index.blade.php
@@ -14,23 +14,25 @@
 	<h1>Planificaci&oacuten semestral para asignaturas UTEM</h1>
 	<div id="login">
         {{ Form::open(array('method' => 'POST', 'url' => '/')) }}
-        {{ Form::label('rut', 'Rut'); }}
+        <p>{{ Form::label('rut', 'Rut'); }}</p>
         {{ Form::text('rut'); }}
-        {{ Form::label('password', 'Contraseña'); }} 
+        <p>{{ Form::label('password', 'Contraseña'); }} </p>
         {{ Form::password('password'); }}
-        {{ Form::label('lblRememberme', 'Recordar contraseña') }}
+        <p>{{ Form::label('lblRememberme', 'Recordar contraseña') }}</p>
         {{ Form::checkbox('rememberme', true) }}
-        {{ Form::submit('Ingresar'); }}
+        <p>{{ Form::submit('Ingresar'); }}</p>
         {{ Form::close() }}
     </div>
-<div id="logo"><img src="Images/utem.jpg"></div>
+    <div id="Inscribir">{{HTML::link('registrar','¿No tiene una cuenta? ¡Regístrese acá!')}}</div>
+<div id="logo"><img src="Images/logo-utem.jpg"></div>
+  <div id="link";>{{HTML::link('admin','Logueo como Admin')}}</div>
 	<div id="foot">
 		<p>UNIVERSIDAD TECNOLOGICA METROPOLITANA</p>
 		<p>Dieciocho 161, Santiago-Chile</p>
 		<div id="link-logo">
 			<a href=http://www.utem.cl><img src ="Images/logo.png"></a>
 		</div>
-        <div id="link";>{{HTML::link('admin','Logueo como Admin')}}</div>
+      
 	</div>
     <div id="linea">
     </div>
